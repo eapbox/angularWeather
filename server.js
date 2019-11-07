ada199e31da9dd04e672d986760860ff01cd7747
Ершов Александр <eapbox@gmail.com>
2019-11-07 16:37:22 +0300

    make it better

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/pogoda'));
 app.listen(process.env.PORT || 8080);
