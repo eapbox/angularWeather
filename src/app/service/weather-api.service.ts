fb9d2288fe7c612399d5286eddec234483ae63fa
Ершов Александр <eapbox@gmail.com>
2019-11-07 16:48:02 +0300

    make it better

--- src/app/service/weather-api.service.ts
+++ src/app/service/weather-api.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class WeatherApiService {
-  private baseURL = 'http://api.openweathermap.org/data/2.5/weather?id=';
+  private baseURL = 'https://api.openweathermap.org/data/2.5/weather?id=';
   private weatherIconURL = 'http://openweathermap.org/img/w/';
   private apiKey = '&units=metric&lang=ru&appid=' + '5460396124d03105c9daf93bcc5f456a';
 
@@ -16,15 +16,7 @@ export class WeatherApiService {
   public getWeather(cityID: string) {
     return new Promise((resolve, reject) => {
       this.http.get(
-        this.baseURL + cityID + this.apiKey,
-        {
-          headers: [
-            {
-              'Content-Security-Policy': 'default-src \'self\'; script-src https://static.ads-twitter.com https://www.google-analytics.com; img-src \'self\' https://s3.amazonaws.com https://twitter.com https://pbs.twimg.com; font-src \'self\' https://fonts.gstatic.com; style-src \'self\' http://api.openweathermap.org; frame-ancestors \'none\';'
-            }
-          ]
-        }
-      ).subscribe(
+        this.baseURL + cityID + this.apiKey).subscribe(
         (data:any) => {
           data.weatherIcon = this.weatherIconURL + data.weather[0].icon + '.png';
 
